fdfcad574ae0cf37647a53bdaf8491f7ee9f5d92
Alexandre <alevaristofig@gmail.com>
2025-08-11 23:51:27 -0300

    Instalação do componente react-icons

--- src/componentes/Cabecalho/index.tsx
+++ src/componentes/Cabecalho/index.tsx
@@ -1,9 +1,19 @@
-import { ReactElement } from "react"
+import { ReactElement } from "react";
+import { Link } from 'react-router-dom';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+import avatarImg from '../../assets/coracao.jpeg';
 
 const Cabecalho = (): ReactElement => {
     return(
         <>
-            <div>Cabeçalho</div>
+            <div className='sidebar'>
+                <div>
+                    <img src={avatarImg} alt='Foto do Usuário'    />            
+                </div>
+            </div>
         </>
     )
 }
