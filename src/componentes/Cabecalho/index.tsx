b20e8cc11fb7ad3545197f063f70fc937ec18ffd
Alexandre <alevaristofig@gmail.com>
2025-09-03 23:09:50 -0300

    Criação do menu peso

--- src/componentes/Cabecalho/index.tsx
+++ src/componentes/Cabecalho/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { FiHome } from 'react-icons/fi';
 import { VscPerson } from "react-icons/vsc";
+import { LiaWeightHangingSolid, LiaRunningSolid  } from 'react-icons/lia';
 
 import styles from './Cabecalho.module.css';
 
@@ -14,6 +15,7 @@ const Cabecalho = (): ReactElement => {
 
     const IconeHome = FiHome as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
     const IconePessoa = VscPerson as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
+    const IconePeso = LiaWeightHangingSolid as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
 
     return(
         <>
@@ -28,6 +30,10 @@ const Cabecalho = (): ReactElement => {
 
                 <Link to="/pessoa">
                     <IconePessoa color="#fff" fontSize={24} /> Pessoa
+                </Link>
+
+                <Link to="/peso/0">
+                    <IconePeso color="#fff" fontSize={24} /> Peso
                 </Link>
             </div>
         </>
