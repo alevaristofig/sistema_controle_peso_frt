6566f95ec3b2bcda227465d9d2efb8a86475e56a
Alexandre <alevaristofig@gmail.com>
2025-09-18 21:57:32 -0300

    Implementações das configurações do redux slice e saga de exercicio

--- src/redux/root-reducer.tsx
+++ src/redux/root-reducer.tsx
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 
 import  pesoSlice  from "./peso/slice";
 import  pessoaSlice  from "./pessoa/slice";
+import  exercicioSlice  from "./exercicio/slice";
 
 const rootReducer = combineReducers({
     peso: pesoSlice,
     pessoa: pessoaSlice,
+    exercicio: exercicioSlice,
 })
 
 
--- src/redux/sagas.tsx
+++ src/redux/sagas.tsx
@@ -2,10 +2,12 @@ import { all } from 'redux-saga/effects';
 
 import peso from '../redux/peso/saga';
 import pessoa from '../redux/pessoa/saga';
+import exercicio from '../redux/exercicio/saga';
 
 export default function* rootSaga() {
     yield all([
         peso,
         pessoa,
+        exercicio,
     ])
 }
