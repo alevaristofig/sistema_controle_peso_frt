82c7f18ecda0103472a2bdb20211af4a57e768bc
Alexandre <alevaristofig@gmail.com>
2025-09-18 21:56:07 -0300

    Implementação do redux slice e saga de exercicio

---
+++ src/redux/exercicio/saga.tsx
@@ -0,0 +1,8 @@
+import { all, takeEvery, put, call } from 'redux-saga/effects';
+import { AnyAction } from 'redux-saga';
+
+import axios, { AxiosResponse } from 'axios';
+
+export default all([
+
+]);
---
+++ src/redux/exercicio/slice.tsx
@@ -0,0 +1,16 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { toast } from 'react-toastify';
+
+const initialState: {}
+
+export const exercicioSlice = createSlice({
+    name: 'exercicio',
+    initialState,
+    reducers: {
+
+    }
+});
+
+export const {} = exercicioSlice.actions;
+
+export default exercicioSlice.reducer;
