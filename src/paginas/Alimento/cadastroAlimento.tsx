146ce42eac0aee13d8f5796b12a6c6e932eb65a3
Alexandre <alevaristofig@gmail.com>
2025-10-27 23:06:04 -0300

    Implmentação da pagina cadastroAlimento

--- src/paginas/Alimento/cadastroAlimento.tsx
+++ src/paginas/Alimento/cadastroAlimento.tsx
@@ -1,6 +1,8 @@
 import { ReactElement, useState } from 'react';
 import { ToastContainer } from "react-toastify";
 
+import useAlimento from '../../hooks/Alimento/alimentoHook';
+
 import Cabecalho from "../../componentes/Cabecalho";
 
 import styles from '../Home/Home.module.css';
@@ -9,10 +11,12 @@ const CadastroAlimento = (): ReactElement => {
 
     const [nome, setNome] = useState<string>('');
     const [quantidade, setQuantidade] = useState<string>('');
-    const [caloria, setCaloria] = useState<number>();
+    const [caloria, setCaloria] = useState<number | string>();
 
-    const mascaraCaloria = (calorias: number): void => {
-        setCaloria();
+    const { formatarCaloria } = useAlimento();
+
+    const mascaraCaloria = (calorias: string): void => {
+        setCaloria(formatarCaloria(calorias));
     }
 
     const salvarAlimento = (): void => {
@@ -31,7 +35,7 @@ const CadastroAlimento = (): ReactElement => {
                         <div className="row mt-3">
                             <div className="col">
                                 <label className="form-label">Nome</label>
-                                <label className="form-label obrigatorio">*</label>
+                                <label className={`form-label ${styles.obrigatorio}`}>*</label>
                                 <input 
                                     type='text'
                                     className="form-control" 
@@ -44,7 +48,7 @@ const CadastroAlimento = (): ReactElement => {
                         <div className="row mt-3">
                             <div className="col">
                                 <label className="form-label">Quantidade</label>
-                                <label className="form-label obrigatorio">*</label>
+                                <label className={`form-label ${styles.obrigatorio}`}>*</label>
                                 <input 
                                     type='text'
                                     className="form-control" 
@@ -57,7 +61,7 @@ const CadastroAlimento = (): ReactElement => {
                         <div className="row mt-3">
                             <div className="col">
                                 <label className="form-label">Calorias</label>
-                                <label className="form-label obrigatorio">*</label>
+                                <label className={`form-label ${styles.obrigatorio}`}>*</label>
                                 <input 
                                     type='text'
                                     className="form-control" 
@@ -65,6 +69,11 @@ const CadastroAlimento = (): ReactElement => {
                                     onChange={(e) => mascaraCaloria(e.target.value)} 
                                     required 
                                 /> 
+                            </div>
+                        </div>
+                        <div className="row mt-3">
+                            <div className="col">
+                                <button type="submit" className="btn btn-primary">Cadastrar</button>
                             </div>
                         </div>
                     </form>
