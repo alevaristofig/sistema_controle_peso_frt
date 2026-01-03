a3c8bc2ed62635c40b2334a6c7e910798010170f
Alexandre <alevaristofig@gmail.com>
2026-01-02 21:30:04 -0300

    Implementação da pagina cadastro peso

--- src/paginas/Peso/cadastroPeso.tsx
+++ src/paginas/Peso/cadastroPeso.tsx
@@ -1,6 +1,7 @@
-import { ReactElement, useState, useEffect } from "react";
+import { ReactElement, useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
+import InputMask from 'react-input-mask';
 
 import { RootState } from "../../redux/root-reducer";
 
@@ -11,9 +12,82 @@ import styles from '../Home/Home.module.css';
 
 const CadastroPeso = (): ReactElement => {
 
+    const [pesoValor,setPesoValor] = useState<number>(0.00);
+    const [imc,setImc] = useState<number>(0.00);
+    const [dataCadastro,setDataCadastro] = useState('');
+    const [dataAtualizacao,setDataAtualizacao] = useState<string>("");
+
+    const inputRef = useRef(null);
+
+    const mascaraPeso = (): void => {
+
+    }
+
+    const calcularImc = (): void => {
+
+    }
+
+    const salvarPeso = (e: React.ChangeEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+    }
+
     return(
         <>
-            <div>Cadastro peso</div>
+            <Cabecalho />
+            <div className={styles.content}>
+                <div>
+                    <ToastContainer />
+                </div> 
+                <div className="container py-4">
+                    <form className="form-perfil" onSubmit={salvarPeso}>
+                        <div className="row mt-3">
+                            <div className="col">
+                                <label className="form-label">Peso</label>
+                                <label className={`form-label ${styles.obrigatorio}`}>*</label>
+                                <input 
+                                    type='text' 
+                                    id='inputPeso'
+                                    className="form-control"  
+                                    value={pesoValor}                                                                                 
+                                    onChange={(e) => mascaraPeso(e.target.value)} 
+                                    onBlur={(e) => calcularImc(e.target.value)}
+                                    required
+                                />    
+                            </div>
+                        </div>
+                        <div className="row mt-3">
+                            <div className="col">
+                                <label className="form-label">Imc</label>
+                                <label className="form-label obrigatorio">*</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control"
+                                    value={imc}                                    
+                                    required />                                
+                            </div>
+                        </div>
+                        <div className="row mt-3">
+                            <div className="col">
+                                <label className="form-label">Data</label>
+                                <label className="form-label obrigatorio">*</label>
+                                <InputMask 
+                                    mask="99/99/9999" 
+                                    name="data" 
+                                    className="form-control" 
+                                    onChange={ e => setDataCadastro(e.target.value)}
+                                    ref={inputRef}
+                                    required
+                                /> 
+                            </div>
+                        </div>
+                        <div className="row mt-3">
+                            <div className="col">
+                                <button type="submit" className="btn btn-primary">Cadastrar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
