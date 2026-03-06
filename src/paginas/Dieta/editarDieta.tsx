48cf836b20ec072e9980361f2bf56f74593fada4
Alexandre <alevaristofig@gmail.com>
2026-03-05 23:38:34 -0300

    Implementação da pagina editar dieta e criação de sua rota

--- src/paginas/Dieta/editarDieta.tsx
+++ src/paginas/Dieta/editarDieta.tsx
@@ -2,7 +2,28 @@ import { ReactElement, useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router';
 import { ToastContainer } from "react-toastify";
 
+import useDieta from '../../hooks/Dieta/dietaHook';
+
+import Cabecalho from "../../componentes/Cabecalho";
+import ModalToken from '../../componentes/Token';
+
+import styles from '../Home/Home.module.css';
+
 const EditarDieta = (): ReactElement => {
+
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    useEffect(() => {
+    
+        if(sessionStorage.getItem('token') == null) {           
+            navigate('/login');
+        }  
+        
+        //buscarAlimento();
+    
+    },[]);
+
     return(
         <>
             <div>Editar Dieta</div>
