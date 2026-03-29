996dec2f8bf22a4eb3092f16b2c5d117e6eee233
Alexandre <alevaristofig@gmail.com>
2026-03-29 18:14:14 -0300

    Criação do hook authuser e do protected route

---
+++ src/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from './hooks/Auth/authHook';
+
+export const ProtectedRoute = ({ children }: any) => {
+    const { isAuthenticated } = useAuth();
+
+    if(!isAuthenticated) {
+        return <Navigate to="/login" replace />
+    }
+
+    return children;
+}
---
+++ src/hooks/Auth/authHook.ts
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch, UseDispatch, useSelector } from "react-redux";
+import { setAuth, logout } from "../../redux/store/authSlice";
+import { authService } from "../../service/auth";
+
+export const useAuth = () => {
+    const dispatch = useDispatch();
+    const auth = useSelector((state: any) => state.auth);
+
+    useEffect(() => {
+        const user = authService.getUser();
+
+        if(user && !auth.isAuthenticated) {
+            dispatch(setAuth(user));
+        }
+
+        if(!user && auth.isAuthenticated) {
+            dispatch(logout());
+        }
+    },[dispatch]);
+
+    return auth;
+}
